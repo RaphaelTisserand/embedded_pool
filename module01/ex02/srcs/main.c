e1ee79c60280d80b1076047d53fe1b821b8f8e19
raph2leretour <tisserandraph@gmail.com>
2025-10-30 18:43:24 +0100

    coaaaa

--- module01/ex02/srcs/main.c
+++ module01/ex02/srcs/main.c
@@ -3,10 +3,10 @@
 int main(void) {
 	DDRB = 1 << DDB1; // PB1 initialisation -> output mod
 
-	OCR1A = 31250; // Set the flag
+	OCR1A = 6250; // Set the flag
 
-	TCCR1A &= ~(1 << COM1A1); // Toggle OC1A when flag gets hit
-	TCCR1A |= (1 << COM1A0);
+	TCCR1A |= (1 << COM1A1); // Clear OC1A when flag gets hit
+	TCCR1A &= ~(1 << COM1A0);
 
 	TCCR1B |= (1 << WGM12) | (1 << CS12); // Divide frequency by 256
 	TCCR1B &= ~((1 << CS11) | (1 << CS10));
